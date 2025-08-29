24e44a5e9e4ad14892a000d5df15e7ac0959eb4a
thisalsalpura <salpurathisal@gmail.com>
2025-08-29 08:46:16 +0530

    Update 1.1

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, Image } from 'react-native';
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
+import { SvgUri } from "react-native-svg";
 
 export default function SignIn() {
     return (
-        <SafeAreaView className="h-full w-full bg-cus-bg">
+        <SafeAreaView className="flex-1 bg-cus-bg">
             <View>
                 <View className="h-20 w-full">
-                    <LinearGradient colors={["#DCB5A4", "#CC876A"]} className="h-full w-full" start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} />
+                    <LinearGradient colors={["#DCB5A4", "#CC876A"]} className="flex-1" start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} />
                 </View>
-                <View className="h-auto w-full py-4 px-8">
+                <View className="h-auto w-full flex flex-row justify-start items-center py-4 px-6 gap-4">
                     <Text className="text-2xl text-black font-gilroyHeavy">My To-Do</Text>
                 </View>
                 <StatusBar style="auto" />
